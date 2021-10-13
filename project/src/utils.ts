ee26330b632ff550abc016afb99e857a55c15e1e
extendsnull <extendsnull@gmail.com>
2021-10-13 21:52:16 +0300

    Рефакторит утилиту

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -1,6 +1,7 @@
+import {generatePath} from 'react-router';
 import {RATING_MAX_VALUE, AppRoute} from './const';
 
-const getOfferUrl = (id: number): string => AppRoute.Offer.replace(':id', id.toString());
+const getOfferUrl = (id: number): string => generatePath(AppRoute.Offer, {id});
 
 const getRatingValue = (rating: number): string => `${Math.ceil((100 / RATING_MAX_VALUE) * rating)}%`;
 
